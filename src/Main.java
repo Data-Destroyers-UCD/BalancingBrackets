e72f5e9bcfe8412cc715dadeee7fdb6584ab23b6
YuBaiBY <baiyu2023by@163.com>
2024-02-07 01:43:30 +0000

    For testing

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I want to be featured in a Stack Program!");
+		System.out.println("This line is for Yu's test");
 
 	}
 
